09567196a825bd56dc407d2f2f8518d957d62b80
Saulo Chiosi <sauloc.1234@gmail.com>
2025-03-24 11:00:52 -0300

    Aplicado Código Limpo e DocString na classe UsuarioSerivce

--- src/main/java/com/course/api_crud/services/UsuarioService.java
+++ src/main/java/com/course/api_crud/services/UsuarioService.java
@@ -15,32 +15,75 @@ public class UsuarioService {
 	@Autowired
 	private UsuarioRepository repository;
 	
+	/**
+	 * Retorna uma lista contendo todos os usuários cadastrados no sistema.
+	 *
+	 * @return Lista de usuários cadastrados.
+	 */
+	
 	public List<Usuario> findAll(){
 		return repository.findAll();
 	}
 	
+	/**
+	 * Busca um usuário pelo ID informado.
+	 *
+	 * @param id Identificador único do usuário.
+	 * @return O usuário correspondente ao ID fornecido.
+	 * @throws NoSuchElementException Se o usuário não for encontrado.
+	 */
+	
 	public Usuario findById(Long id) {
-		Optional<Usuario> usu = repository.findById(id);
-		return usu.get();
+		Optional<Usuario> usuario = repository.findById(id);
+		return usuario.get();
 	}
 	
-	public Usuario insert(Usuario usu) {
-		return repository.save(usu);
+	/**
+	 * Insere um novo usuário no banco de dados.
+	 *
+	 * @param usuario Objeto contendo os dados do usuário a ser cadastrado.
+	 * @return O usuário salvo com o ID gerado.
+	 */
+	
+	public Usuario insert(Usuario usuario) {
+		return repository.save(usuario);
 	}
+	
+	/**
+	 * Exclui um usuário do banco de dados com base no ID informado.
+	 *
+	 * @param id Identificador único do usuário a ser excluído.
+	 * @throws EmptyResultDataAccessException Se o ID não existir no banco de dados.
+	 */
 	
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
 	
-	public Usuario update(Long id, Usuario usu) {
-		Usuario entidade = repository.getReferenceById(id);
-		updateData(entidade, usu);
-		return repository.save(entidade);
+	/**
+	 * Atualiza os dados de um usuário existente com base no ID informado.
+	 *
+	 * @param id Identificador único do usuário a ser atualizado.
+	 * @param usuario Objeto contendo os novos dados do usuário.
+	 * @return O usuário atualizado e salvo no banco de dados.
+	 * @throws EntityNotFoundException Se o usuário com o ID fornecido não for encontrado.
+	 */
+	
+	public Usuario update(Long id, Usuario usuario) {
+		Usuario usuarioAtualizado = repository.getReferenceById(id);
+		updateData(usuarioAtualizado, usuario);
+		return repository.save(usuarioAtualizado);
 	}
 
-	private void updateData(Usuario entidade, Usuario usu) {
-		entidade.setName(usu.getName());
-		entidade.setEmail(usu.getEmail());
-		
+	/**
+	 * Atualiza os dados de um usuário existente com as informações fornecidas.
+	 *
+	 * @param usuarioAtualizado Usuário que será atualizado.
+	 * @param usuario Objeto contendo os novos dados do usuário.
+	 */
+	
+	private void updateData(Usuario usuarioAtualizado, Usuario usuario) {
+		usuarioAtualizado.setName(usuario.getName());
+		usuarioAtualizado.setEmail(usuario.getEmail());
 	}
 }
